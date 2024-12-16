53e5168390b461f16c6bb2b5e334fea2c17df1c9
lhnguyenVP21 <lhnguyen21@vp.fitus.edu.vn>
2024-12-16 18:41:33 +0700

    edit the + button from main frame

--- ELA/main.py
+++ ELA/main.py
@@ -9,7 +9,7 @@ current_image = None
 ela_image = None
 original_photo = None
 ela_photo = None
-current_mode = "RGB" 
+current_mode = "RGB"
 current_file_path = None
 exif_visible = False
 
@@ -35,6 +35,10 @@ def open_image():
 
     display_images()
     display_exif_info()
+
+def hide_button():
+    add_image_button.grid_remove()
+    open_image()
 
 def select_mode(mode):
     global current_mode, current_image, ela_image, original_photo, ela_photo
@@ -67,12 +71,11 @@ def display_images():
     original_label.config(image=original_photo)
     ela_label.config(image=ela_photo)
 
-    add_image_button.pack_forget()
+    add_image_button.grid_remove()
     left_frame.pack(side=tk.LEFT, fill=tk.Y, padx=10, pady=10)
     right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True, padx=10, pady=10)
 
 def update_ela(*args):
-    """Updates the ELA image when slider values change."""
     if current_image is not None:
         global ela_image, ela_photo
         ela_image = perform_ela(current_image, jpeg_quality_slider.get(), error_scale_slider.get())
@@ -80,6 +83,7 @@ def update_ela(*args):
             return
         ela_photo = ImageTk.PhotoImage(ela_image.resize((400, 400)))
         ela_label.config(image=ela_photo)
+
 def toggle_exif_visibility():
     global exif_visible
     exif_visible = not exif_visible
@@ -96,9 +100,12 @@ root.title("Image Analysis Tool: ELA and EXIF")
 root.geometry("1400x800")
 root.config(bg="white")
 
-add_image_button = tk.Button(root, text="+", font=("Arial", 50), fg="black", bg="lightgrey", command=open_image)
-add_image_button.pack(pady=50)
+add_image_button = tk.Button(root, text="Click anywhere to add image", font=("Arial", 25), fg="black", bg="lightgrey",
+                             command=hide_button, width=2, height=2)
+add_image_button.grid(row=0, column=0, sticky="nsew")
 
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
 left_frame = tk.Frame(root, bg="white", width=300, padx=10, pady=10)
 
@@ -111,15 +118,14 @@ rgb_button.pack(side=tk.LEFT, padx=5)
 l_button = tk.Button(mode_frame, text="Grayscale (L)", command=lambda: select_mode("L"), width=10)
 l_button.pack(side=tk.LEFT, padx=5)
 
-
 jpeg_quality_label = tk.Label(left_frame, text="JPEG Quality", font=("Arial", 10), bg="white")
-jpeg_quality_label.pack(pady=(20,5))
+jpeg_quality_label.pack(pady=(20, 5))
 jpeg_quality_slider = tk.Scale(left_frame, from_=0, to=100, orient=tk.HORIZONTAL, command=update_ela, length=400)
 jpeg_quality_slider.set(75)
 jpeg_quality_slider.pack()
 
 error_scale_label = tk.Label(left_frame, text="Error Scale", font=("Arial", 10), bg="white")
-error_scale_label.pack(pady=(20,5))
+error_scale_label.pack(pady=(20, 5))
 error_scale_slider = tk.Scale(left_frame, from_=1, to=50, orient=tk.HORIZONTAL, command=update_ela, length=400)
 error_scale_slider.set(10)
 error_scale_slider.pack()
@@ -137,12 +143,10 @@ original_label.pack(side=tk.LEFT, padx=10)
 ela_label = tk.Label(image_frame)
 ela_label.pack(side=tk.RIGHT, padx=10)
 
-
 toggle_exif_button = tk.Button(right_frame, text="Show EXIF", command=toggle_exif_visibility, width=20)
 toggle_exif_button.pack(pady=10)
 
 exif_frame = tk.LabelFrame(right_frame, text="EXIF Information", bg="white", padx=10, pady=10, width=400)
-# exif_frame.pack(fill=tk.BOTH, expand=True,pady=20)
 
 exif_text = scrolledtext.ScrolledText(exif_frame, wrap=tk.WORD, width=30, height=10, state=tk.DISABLED)
 exif_text.pack(fill=tk.BOTH, expand=True)
